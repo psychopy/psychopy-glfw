828ea0f56667afde4b0a4f985c417378dea1f08b
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-08-07 17:15:30 -0400

    FF: Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ name = "psychopy-glfw"
 
 # Version for the package. Use setuptools conventions for specifying the version
 # string.
-version = "0.1"
+version = "0.0.1"
 
 # Define packages to be included with this plugin.
 packages = [
