3ae680f4116822aa77fb03b622338e120da9f933
Jonathan Peirce <jon.peirce@gmail.com>
2024-06-18 15:19:14 +0100

    Moving to dynamic versioning with setuptools_scm

--- psychopy_glfw/__init__.py
+++ psychopy_glfw/__init__.py
@@ -1,1 +1,10 @@
 from .glfwbackend import GLFWBackend
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+del version, PackageNotFoundError  # no longer needed and confusing for users?
